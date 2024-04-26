a5a6f684a132c93b63fb4ceeba6d4d2768dd1120
Vitor André Gehrke <vitor.gehrke@gmail.com>
2024-04-26 18:30:09 -0300

    Corrigida anotação do Editor para referenciar a pfk (referencedColumnName incorreta)

--- src/main/java/com/ajmv/altoValeNewsBackend/model/Editor.java
+++ src/main/java/com/ajmv/altoValeNewsBackend/model/Editor.java
@@ -6,9 +6,9 @@ import lombok.NoArgsConstructor;
 
 @Table(name = "editor")
 @Entity(name = "editor")
-//@NoArgsConstructor
-@AllArgsConstructor
-@PrimaryKeyJoinColumn(name = "editor_id", referencedColumnName = "userId")
+@NoArgsConstructor
+//@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "editor_id", referencedColumnName = "vip_id")
 public class Editor extends UsuarioVIP{
 
 }
