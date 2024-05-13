413c0c8c1e80694dddb7668f69734d6487f2bf4e
AndreHLudwig <andre.udesc@hotmail.com>
2024-05-12 21:39:29 -0300

    porta adicionada
    php: 80

--- src/main/java/com/ajmv/altoValeNewsBackend/WebConfig.java
+++ src/main/java/com/ajmv/altoValeNewsBackend/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://localhost:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
